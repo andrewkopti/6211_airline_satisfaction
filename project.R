f8db202eb19aec4d15364199a0695fc5e1e27471
tweston4 <103375000+tweston4@users.noreply.github.com>
2022-04-26 17:57:11 -0400

    Update project.R
    Added preliminary models random forest and regression

--- project.R
+++ project.R
@@ -2,6 +2,19 @@ library(tidyverse)
 library(ggplot2)
 library(GGally)
 
+library(VIM)
+library(mice)
+library(caret)
+library(factoextra)
+library(randomForest)
+library(ggcorrplot)
+library(caret)
+library(pROC)
+library(rpart)
+library(rpart.plot)
+library(dplyr)
+
+#setwd('')
 filename <- "data/passenger_satisfaction.csv"
 df <- read_csv(filename)
 
@@ -175,3 +188,162 @@ sd(df$departure_delay)
 arrival_delay <- drop_na(df)
 
 sd(arrival_delay$arrival_delay)
+
+# Travis W.
+
+# Set the random seed
+set.seed(100)
+
+summary(df)
+str(df)
+head(df)
+
+# Handle Missing Data (MICE)
+ar_delay_avg <- mean(df$arrival_delay, na.rm = TRUE)
+
+md.pattern(df, rotate.names = TRUE)
+
+aggr_plot <- aggr(df, col=c("forestgreen","darkgoldenrod1"), numbers=TRUE, sortVars=TRUE, labels=names(df), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
+
+mice_res <- mice(df, method = 'rf', maxit= 10)
+
+summary(mice_res$data)
+imp_df <- complete(mice_res,1)
+summary(imp_df)
+
+xyplot(mice_res,arrival_delay ~ departure_delay,pch=18,cex=1)
+
+# Data Plots 
+ggplot(imp_df, aes(x=reorder(satisfaction, satisfaction, function(x)-length(x)))) +
+  geom_bar(fill=c("forestgreen","darkgoldenrod1")) +  labs(x='Satisfaction')
+
+# Stacked
+ggplot(imp_df, aes(fill=cleanliness, x=satisfaction)) + 
+  geom_bar(position="stack")
+
+ggplot(imp_df, aes(fill=gender, x=satisfaction)) + 
+  geom_bar(position="stack")
+
+ggplot(imp_df, aes(fill=class, x=satisfaction)) + 
+  geom_bar(position="stack")
+
+
+ggplot(imp_df, aes(fill=checkin_service, x=satisfaction)) + 
+  geom_bar(position="stack")
+
+ggplot(imp_df, aes(x=satisfaction, y=departure_delay)) +  geom_boxplot(fill=c("forestgreen","darkgoldenrod1"))
+
+ggplot(imp_df, aes(x=satisfaction, y=flight_distance)) +  geom_boxplot(fill=c("forestgreen","darkgoldenrod1"))
+
+
+chisq.test(imp_df$travel_type,imp_df$satisfaction)
+
+numeric_df <- dplyr::select_if(imp_df, is.numeric)
+
+r <- cor(numeric_df,use="complete.obs")
+
+ggcorrplot(r)
+
+# Models
+
+
+# Dimension Reduction 
+df.train.numeric <- imp_df[22:23]
+pca <- prcomp(df.train.numeric,center=TRUE,scale.=TRUE)
+print(pca)
+plot(pca)
+summary(pca)
+
+fviz_eig(pca)
+fviz_pca_var(pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+pred_pca <- predict(pca,newdata = df.train.numeric)
+
+df.pca <- cbind.data.frame(imp_df[,c(2:21,24)],pred_pca[,1])
+colnames(df.pca)[22] <- "delay_pca"
+# Standard Partition with imputed data
+trainIndex <-
+  createDataPartition(imp_df$satisfaction,
+                      p = .7,
+                      list = FALSE,
+                      times = 1)
+
+df.train <- imp_df[trainIndex, ]
+df.valid <- imp_df[-trainIndex, ]
+# PCA Partition with Imputed data
+trainIndex <-
+  createDataPartition(df.pca$satisfaction,
+                      p = .7,
+                      list = FALSE,
+                      times = 1)
+
+pca.train <- df.pca[trainIndex, ]
+pca.valid <- df.pca[-trainIndex, ]
+
+# Logistic Regression
+base.model <- train(satisfaction~. -id
+                    ,data = df.train
+                    ,method = "glm"
+                    ,family = "binomial")
+summary(base.model)
+pca.base.model <- train(satisfaction~.
+                        ,data = pca.train
+                        ,method = "glm"
+                        ,family = "binomial")
+summary(pca.base.model)
+
+
+# Score base model
+prediction <-
+  predict(base.model, newdata = df.valid, na.action = na.pass)
+confusionMatrix(prediction, df.valid$satisfaction)
+
+# Score pca base model
+prediction <-
+  predict(pca.base.model, newdata = pca.valid, na.action = na.pass)
+confusionMatrix(prediction, pca.valid$satisfaction)
+
+trControl <- trainControl(method = 'cv',
+                          number = 10,
+                          search = 'grid')
+
+# Random Forest Model
+tuneGrid <- expand.grid(.mtry = c(1:15))
+rf <- train(
+  satisfaction~.,
+  data = pca.train,
+  method = "rf",
+  metric = "Accuracy",
+  tuneGrid = tuneGrid,
+  trControl = trControl,
+  ntree = 300
+)
+
+rf
+prediction <-
+  predict(rf, newdata = pca.valid, na.action = na.pass)
+confusionMatrix(prediction, pca.valid$satisfaction)
+
+
+# SVM Model
+# grid <- expand.grid(sigma = seq(0,2,by=.25),C = c(0.005,0.01,0.05, 0.25, 0.5, 0.75, 1))
+grid <- expand.grid(C = c(0.005, 0.01, 0.05, 0.25, 0.5, 0.75, 1))
+
+svm <- train(
+  satisfaction ~ .,
+  data = pca.train,
+  method = 'svmLinear',
+  trControl = trControl,
+  #preProcess = c('center', 'scale'),
+  tuneGrid = grid
+)
+
+svm
+prediction <-
+  predict(rf, newdata = pca.valid, na.action = na.pass)
+confusionMatrix(prediction, pca.valid$satisfaction)
+
